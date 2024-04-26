0abdcaf1c594a101fe5b3ffb21495472fae1a4d7
lyl <845602274@qq.com>
2024-04-26 20:01:32 +0800

    update eval

---
+++ agents/utils_eval.py
@@ -0,0 +1,48 @@
+import pathlib
+import torch
+from pogema.animation import AnimationConfig, AnimationMonitor
+from pomapf_env.env import make_pomapf
+from pomapf_env.pomapf_config import POMAPFConfig
+
+
+def eval_algorithm(algo, map_name='sc1-AcrosstheCape', max_episode_steps=512, seed=None, num_agents=64, animate=False):
+    gc = POMAPFConfig(map_name=map_name, max_episode_steps=max_episode_steps, seed=seed, num_agents=num_agents)
+    env = make_pomapf(grid_config=gc)
+    algo_name = type(algo).__name__
+    if animate:
+        anim_dir = str(pathlib.Path('renders') / algo_name)
+        env = AnimationMonitor(env, AnimationConfig(egocentric_idx=0 , directory=anim_dir))
+    obs, infos = env.reset()
+    algo.after_reset()
+    results_holder = ResultsHolder()
+
+    dones = [False for _ in range(len(obs))]
+    rew = [0 for _ in range(len(obs))]
+    actions = algo.act(obs, rew, dones, infos)
+
+    with torch.no_grad():
+        while True:
+            obs, rew, dones, truncated, infos = env.step(actions)
+            results_holder.after_step(infos)
+            algo.after_step(dones)
+
+            if all(dones) or all(truncated):
+                break
+
+            actions = algo.act(obs, rew, dones, infos)
+
+    results = results_holder.get_final()
+    results['algorithm'] = algo_name
+    return results
+
+
+class ResultsHolder:
+    def __init__(self):
+        self.results = dict()
+
+    def after_step(self, infos):
+        if 'metrics' in infos[0]:
+            self.results.update(**infos[0]['metrics'])
+
+    def get_final(self):
+        return self.results
--- eval.py
+++ eval.py
@@ -1,10 +1,19 @@
 import os
+from pathlib import Path
 from prettytable import PrettyTable
 from pomapf_env.custom_maps import MAPS_REGISTRY , _test_regexp
 import re
 import time
-# from agents.epom import example_epom
-from agents.replan import example_replan
+from agents.epom import EPOM , EpomConfig
+from agents.replan import RePlan , RePlanConfig
+from agents.utils_eval import eval_algorithm
+
+def get_algo_by_name(algo_name):
+    if algo_name == "Replan":
+        algo = RePlan(RePlanConfig(max_planning_steps=10000))
+    elif algo_name == "EPOM":
+        algo = EPOM(EpomConfig(path_to_weights=str('./' / Path('weights/epom'))))
+    return algo
 
 def get_test_maps():
     test_maps = []
@@ -28,24 +37,27 @@ def main():
     # os.environ['OMP_NUM_THREADS'] = "1"
     # os.environ['MKL_NUM_THREADS'] = "1"
     current_time = time.strftime('%Y_%m_%d_%H_%M_%S',time.localtime(time.time()))
-    run_examples_funcs = {
-        "Replan" : example_replan,
-        #"EPOM" : example_epom,
-    }
+    algo_list = ["Replan" , "EPOM"]
 
     score_table = PrettyTable()
     score_table.field_names = ["Algorithm", "Num of Agents","Avg ISR", "Avg CSR", "Avg Episode Length"]
 
     test_maps = get_test_maps()
-    for algo_name , algo_func in run_examples_funcs.items():
+    for algo_name in algo_list:
+        print(f"current algo_name: {algo_name}")
+        algo = get_algo_by_name(algo_name)
         for num_agents in [64 , 128 , 192 , 256, 320, 384]:
+            print(f"current num_agents: {num_agents}")
             _step = 0
             _ISR = 0
             _CSR = 0
             _ep_length = 0
-            for seed in range(0,50):
+            for seed in range(0,10):
+                print(f"current seed: {seed}")
                 for map_name in test_maps:
-                    result = algo_func(map_name=map_name, num_agents=num_agents, seed=seed, max_episode_steps=512, animate=False)
+                    print(f"current map: {map_name}")
+                    result = eval_algorithm(algo=algo , map_name=map_name, num_agents=num_agents, seed=seed, max_episode_steps=512, animate=False)
+                    algo.after_reset()
                     _step = _step + 1
                     _ISR = _ISR + result['ISR']
                     _CSR = _CSR + result['CSR']
@@ -54,7 +66,7 @@ def main():
             score_table.add_row([algo_name, num_agents , _ISR/_step, _CSR/_step, _ep_length/_step])
             new_row = score_table.get_string(start=len(score_table._rows) - 1, end=len(score_table._rows))
             print(new_row)
-            write_into_file(score_table, current_time)
+            write_into_file(score_table, algo_name, current_time)
     print(score_table)
 
 
