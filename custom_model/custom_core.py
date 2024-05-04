e63ba74e13c05282d254ab5c345c70e1b4e96f00
lyl <845602274@qq.com>
2024-05-04 14:59:48 +0800

    update pooling

--- custom_model/custom_core.py
+++ custom_model/custom_core.py
@@ -28,7 +28,17 @@ class CustomCore(ModelCore):
 
         head_output = head_output.view(-1, self.memory_length, self.input_size)
         x = self.core(head_output)
+        
         # pooling
-        x = F.adaptive_max_pool1d(x.permute(0, 2, 1), output_size=1).squeeze(dim=-1)
+        # 定义权重参数
+        weights = torch.arange(1, self.memory_length+1, dtype=torch.float)  # 从1到5的递增序列
+        # 将权重参数转换为与输入张量的形状相匹配的张量
+        weights = weights.unsqueeze(0).unsqueeze(-1)
+        # 对权重参数进行归一化，确保权重之和为1
+        weights = F.softmax(weights, dim=1)
+        # 将权重参数广播到与输入张量相同的形状
+        weights = weights.expand(x.size(0), -1, -1)
+        x = x * weights
+        x = F.adaptive_avg_pool1d(x.permute(0, 2, 1), output_size=1).squeeze(dim=-1)
 
         return x, fake_rnn_states
